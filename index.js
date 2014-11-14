5eddb9a797500f80d9eb22d73888ece3aa82265f
chrisdevwords <chris@1stdibs.com>
2014-11-14 14:36:09 -0500

    writes minified file by flag only

--- index.js
+++ index.js
@@ -9,26 +9,34 @@ var pkg = require(path.join(__dirname, 'package.json'));
 program
     .version(pkg.version)
     .option('-f, --file <file>', 'File which you want to minify and copy')
-    .option('-o, --output <file>', 'Minified file name (optional: defaults to <file>.min.<extension>)')
+    .option('-w, --write', 'Write a minified file to the file system.')
+    .option('-d, --dest <file>', 'Minified file name/location (optional: defaults to <file>.min.<extension>)')
     .parse(process.argv);
 
 var file = program.file || '';
+var doWrite = program.write;
 var extension = file.split('.').slice(-1)[0].toLowerCase();
-var output = program.output || file.slice(0, -1 * extension.length) + 'min.' + extension;
+var output = program.dest || file.slice(0, -1 * extension.length) + 'min.' + extension;
 
 switch (extension) {
     case "js":
     case "css":
         minify(file, {returnStream  : true}, function(error, stream) {
-            var streamWrite = fs.createWriteStream(output);
-            //console.log(stream);
+            if (doWrite) {
+                fs.createWriteStream(output);
+            }
             if (error) {
-                console.error(error.message);
+                console.log(error.message);
             } else {
                 copyPaste.copy(stream, function() {
-                    console.log('Copied minified file', file, 'with extension', extension, 'output as', output);
+                    console.log('Copied minified file', file, 'to clipboard.');
+                    if(doWrite){
+                        console.log('Output saved to', output);
+                    }
                 });
             }
         });
     break;
 }
+
+
